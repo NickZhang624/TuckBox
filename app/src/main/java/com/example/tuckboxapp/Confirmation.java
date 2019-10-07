105f7bd4410970f8ecd8dc96f85b04753a6e79bf
lznick.zhang <lznick.zhang@gmail.com>
2019-10-07 15:29:38 +1300

    Tuck Box

--- app/src/main/java/com/example/tuckboxapp/Confirmation.java
+++ app/src/main/java/com/example/tuckboxapp/Confirmation.java
@@ -5,13 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Confirmation extends Menu {
+
+    TextView tvRgion,tvNote,tvTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        Intent i = getIntent();
+        String region =i.getStringExtra(PlaceOrder.EXTRA_REGION);
+        String note = i.getStringExtra(PlaceOrder.EXTRA_NOTE);
+        String time = i.getStringExtra(PlaceOrder.EXTRA_TIME);
+
+        tvRgion =findViewById(R.id.con_region);
+        tvNote =findViewById(R.id.con_note);
+        tvTime =findViewById(R.id.con_time);
+
+        tvRgion.setText("Delivery Region: " + region);
+        tvNote.setText("Customer Notes: " + note);
+        tvTime.setText("Delivery Time: " + time);
     }
 
     public void confirmCancelButtonClicked(View view) {
--- app/src/main/java/com/example/tuckboxapp/PlaceOrder.java
+++ app/src/main/java/com/example/tuckboxapp/PlaceOrder.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -17,18 +18,29 @@ public class PlaceOrder extends Menu {
     Spinner spinnerRegion, spinnerTime;
     EditText etNote;
     TextView evPayment, evAddress;
+    Button buttonCancel,buttonNext;
+
+    public static final String EXTRA_REGION ="EXTRA_REGION";
+    public static final String EXTRA_NOTE ="EXTRA_NOTE";
+    public static final String EXTRA_TIME ="EXTRA_TIME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_order);
-        user = (User) getIntent().getSerializableExtra(MainActivity.USER_OBJECT);
+//        user = (User) getIntent().getSerializableExtra(MainActivity.USER_OBJECT);
+//
+//        insertionResult = -1;
+//
+//        evPayment =findViewById(R.id.order_show_credit_card);
+//        evAddress =findViewById(R.id.order_show_address);
 
-        insertionResult = -1;
-        etNote =findViewById(R.id.note);
-        evPayment =findViewById(R.id.order_show_credit_card);
-        evAddress =findViewById(R.id.order_show_address);
+
+
+        buttonCancel = findViewById(R.id.btn_order_cancel);
+        buttonNext =findViewById(R.id.btn_next_order);
         spinnerRegion =findViewById(R.id.spinner_order_region);
+        etNote =findViewById(R.id.note);
         spinnerTime = findViewById(R.id.spinner_time);
     }
 
@@ -38,7 +50,13 @@ public class PlaceOrder extends Menu {
     }
 
     public void placeOrderNextButtonClicked(View view) {
+        String region = spinnerRegion.getSelectedItem().toString();
+        String note =etNote.getText().toString();
+        String time = spinnerTime.getSelectedItem().toString();
         Intent i = new Intent(this,Confirmation.class);
+        i.putExtra(EXTRA_REGION,region);
+        i.putExtra(EXTRA_NOTE,note);
+        i.putExtra(EXTRA_TIME,time);
         startActivity(i);
     }
 }
